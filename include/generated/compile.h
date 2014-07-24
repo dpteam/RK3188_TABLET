b1beeac3e24a00f241b5b839abb86c8051a88005
Dee-UK <darrellr@sky.com>
2014-07-24 12:06:18 +0100

    Base Config

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jul 24 11:59:49 BST 2014"
+#define LINUX_COMPILE_BY "darrell"
+#define LINUX_COMPILE_HOST "Gargleblaster"
+#define LINUX_COMPILER "gcc version 4.8.2 (Ubuntu/Linaro 4.8.2-16ubuntu4) "
